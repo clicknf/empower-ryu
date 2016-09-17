93832761d7e2e74f87a6d23b8f418be70d0ea714
Roberto Riggio <roberto.riggio@gmail.com>
2016-09-17 21:36:54 +0200

    fix handling bcast frames

--- ryu/app/intent.py
+++ ryu/app/intent.py
@@ -17,6 +17,7 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
+from ryu.lib.mac import haddr_to_bin
 from ryu.ofproto import ofproto_v1_0
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
@@ -30,6 +31,7 @@ from collections import OrderedDict
 OFP_LW_PRIORITY = 100
 OFP_RULE_PRIORITY = 200
 BASE_HEX = 16
+
 
 def dijkstra(vertices, edges, source):
     """Compute minimum spanning tree."""
@@ -258,20 +260,17 @@ class Intent(app_manager.RyuApp):
 
             datapath.send_msg(mod)
 
-    def _add_flow(self, datapath, in_port, dst, actions):
-        """Push flowmode to switch."""
-
+    def add_flow(self, datapath, in_port, dst, actions):
         ofproto = datapath.ofproto
 
         match = datapath.ofproto_parser.OFPMatch(
-            in_port=in_port, dl_dst=dst)
+            in_port=in_port, dl_dst=haddr_to_bin(dst))
 
         mod = datapath.ofproto_parser.OFPFlowMod(
             datapath=datapath, match=match, cookie=0,
             command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-            priority=OFP_LW_PRIORITY,
+            priority=ofproto.OFP_DEFAULT_PRIORITY,
             flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
-
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -295,21 +294,14 @@ class Intent(app_manager.RyuApp):
         dst = eth.dst
         src = eth.src
 
-        # Ignore packets with multicast ethernet destination address
-        if int(dst.split(':')[0], 16) & 1:
+        if int(dst.split(':')[0], BASE_HEX) & 1 and dst != "ff:ff:ff:ff:ff:ff":
+            # ignore multicast packets
             return
 
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        # Ignore packets with multicast ethernet destination address
-
-        if int(dst.split(':')[0], BASE_HEX) & 1:
-            return
-
-        if dst != "ff:ff:ff:ff:ff:ff":
-            self.logger.info("Packet in %s %s %s %s", dpid, src, dst,
-                             msg.in_port)
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = msg.in_port
@@ -317,35 +309,35 @@ class Intent(app_manager.RyuApp):
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
-            if dst != "ff:ff:ff:ff:ff:ff":
-                self.logger.info("Dst %s not found in Mac To Port", dst)
             out_port = ofproto.OFPP_FLOOD
 
-        actions = [datapath.ofproto_parser.OFPActionStripVlan(),  datapath.ofproto_parser.OFPActionOutput(out_port)]
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            self._add_flow(datapath, msg.in_port, dst, actions)
+            self.add_flow(datapath, msg.in_port, dst, actions)
 
         data = None
-
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
         out = datapath.ofproto_parser.OFPPacketOut(
             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
             actions=actions, data=data)
-
         datapath.send_msg(out)
 
-    @set_ev_cls(EventSwitchEnter, MAIN_DISPATCHER)
-    def _switch_enter_handler(self, ev):
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    def _port_status_handler(self, ev):
+        msg = ev.msg
+        reason = msg.reason
+        port_no = msg.desc.port_no
 
-        datapath = ev.switch.dp
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        match = parser.OFPMatch()
-        mod = parser.OFPFlowMod(datapath=datapath, match=match,
-                                cookie=0, command=ofproto.OFPFC_DELETE,
-                                actions=[])
-        datapath.send_msg(mod)
+        ofproto = msg.datapath.ofproto
+        if reason == ofproto.OFPPR_ADD:
+            self.logger.info("port added %s", port_no)
+        elif reason == ofproto.OFPPR_DELETE:
+            self.logger.info("port deleted %s", port_no)
+        elif reason == ofproto.OFPPR_MODIFY:
+            self.logger.info("port modified %s", port_no)
+        else:
+            self.logger.info("Illeagal port state %s %s", port_no, reason)
